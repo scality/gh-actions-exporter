91b0a3856d868cf017eeb6edd4ee40a8b52aa48a
gaspardmoindrot <gaspard.moindrot@scality.com>
2023-05-15 22:34:37 +0000

    [PTFE-168] ✨ Get job cost & unit testing

--- tests/unit/test_cost.py
+++ tests/unit/test_cost.py
@@ -6,4 +6,4 @@ def test_get_job_cost(workflow_job, settings):
 
 
 def test_job_cost_unknown_label(workflow_job, settings):
-    assert Cost(settings).get_job_cost(workflow_job, "xxx") == 0.08
+    assert Cost(settings).get_job_cost(workflow_job, "xxx") == 0
