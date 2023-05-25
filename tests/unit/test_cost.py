6cfb3f9310deae0e4279b8eb34690a6a6132115f
gaspardmoindrot <gaspard.moindrot@scality.com>
2023-05-25 23:10:02 +0000

    [PTFE-217] 🔥 Delete flavor cost

--- tests/unit/test_cost.py
+++ tests/unit/test_cost.py
@@ -2,7 +2,7 @@ from gh_actions_exporter.cost import Cost
 
 
 def test_get_job_cost(workflow_job, settings):
-    assert Cost(settings).get_job_cost(workflow_job, "large") == 0.16
+    assert Cost(settings).get_job_cost(workflow_job, "ubuntu-22.04") == 0.08
 
 
 def test_job_cost_unknown_label(workflow_job, settings):
